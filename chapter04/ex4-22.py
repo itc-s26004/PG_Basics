3dc19d50921f951d62bfeb4e0c868d27d5a0e1c1
itc-s26004 <s26004@std.it-college.ac.jp>
2026-06-03 07:58:46 +0000

    第4章 例題24まで

---
+++ chapter04/ex4-22.py
@@ -0,0 +1,5 @@
+try:
+    10 / 0
+    c = "I will never get defind."
+except ZeroDivisionError:
+    print(c)
---
+++ chapter04/ex4-23.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    """
+    2つの引数の和を返す関数add
+    :引数その1：int x
+    :引数その2：int y
+    :戻り値：x + y
+    """
+    return x + y
