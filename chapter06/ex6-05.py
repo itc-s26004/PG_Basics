8d9b2cf84b28a5e2f30102ddcca76459f4c634cc
itc-s26004 <s26004@std.it-college.ac.jp>
2026-06-16 07:58:13 +0000

    第6章例題終了

--- chapter06/ex6-05.py
+++ chapter06/ex6-05.py
@@ -1,3 +1,5 @@
 ff = "F. Fitzgerald"
 ff = "F. Scott Fitzgerald"
 print(ff)
+
+#文字列にインデックス番号を指定して代入できない
---
+++ chapter06/ex6-10.py
@@ -0,0 +1,8 @@
+what = input("何が:")
+when = input("いつ:")
+where = input("どこで:")
+do = input("どうした:")
+
+r = f"{what}は{when}に{where}で{do}。"
+print(r)
+print(f"{what}は{when},{where}で{do}。")
